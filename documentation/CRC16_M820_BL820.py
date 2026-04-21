d2aca64807834a547b84e96f106c8a0e1cd51d0d
stancecoke <31093785+stancecoke@users.noreply.github.com>
2026-04-21 12:58:45 +0200

    Update CRC16_M820_BL820.py

--- documentation/CRC16_M820_BL820.py
+++ documentation/CRC16_M820_BL820.py
@@ -1,6 +1,64 @@
 import crcmod
 from tkinter import filedialog
+custom_crc_table = [0] * 256  # Initialisierung der Tabelle
+def generate_crc32_table(_poly):
 
+    global custom_crc_table
+
+    for i in range(256):
+        c = i << 24
+
+        for j in range(8):
+            c = (c << 1) ^ _poly if (c & 0x80000000) else c << 1
+
+        custom_crc_table[i] = c & 0xffffffff
+
+
+def crc32_stm(bytes_arr):
+
+    length = len(bytes_arr)
+    crc = 0xffffffff
+
+    k = 0
+    while length >= 4:
+
+        v = ((bytes_arr[k] << 24) & 0xFF000000) | ((bytes_arr[k+1] << 16) & 0xFF0000) | \
+        ((bytes_arr[k+2] << 8) & 0xFF00) | (bytes_arr[k+3] & 0xFF)
+
+        crc = ((crc << 8) & 0xffffffff) ^ custom_crc_table[0xFF & ((crc >> 24) ^ v)]
+        crc = ((crc << 8) & 0xffffffff) ^ custom_crc_table[0xFF & ((crc >> 24) ^ (v >> 8))]
+        crc = ((crc << 8) & 0xffffffff) ^ custom_crc_table[0xFF & ((crc >> 24) ^ (v >> 16))]
+        crc = ((crc << 8) & 0xffffffff) ^ custom_crc_table[0xFF & ((crc >> 24) ^ (v >> 24))]
+
+        k += 4
+        length -= 4
+
+    if length > 0:
+        v = 0
+
+        for i in range(length):
+            v |= (bytes_arr[k+i] << 24-i*8)
+
+        if length == 1:
+            v &= 0xFF000000
+
+        elif length == 2:
+            v &= 0xFFFF0000
+
+        elif length == 3:
+            v &= 0xFFFFFF00
+
+        crc = (( crc << 8 ) & 0xffffffff) ^ custom_crc_table[0xFF & ( (crc >> 24) ^ (v ) )];
+        crc = (( crc << 8 ) & 0xffffffff) ^ custom_crc_table[0xFF & ( (crc >> 24) ^ (v >> 8) )];
+        crc = (( crc << 8 ) & 0xffffffff) ^ custom_crc_table[0xFF & ( (crc >> 24) ^ (v >> 16) )];
+        crc = (( crc << 8 ) & 0xffffffff) ^ custom_crc_table[0xFF & ( (crc >> 24) ^ (v >> 24) )];
+
+    return crc
+
+poly = 0x04C11DB7
+
+
+generate_crc32_table(poly)
 # Definiere den CRC-Typ (z.B. CRC-16-CCITT-FALSE)
 # '0x1021' ist das Polynom, '0xFFFF' der Initialwert, 'True' das XOR-Ergebnis nachher
 crc16_func = crcmod.mkCrcFun(0x11021, initCrc=0x0000, rev=False, xorOut=0x0000)
@@ -9,25 +67,23 @@ file_path = filedialog.askopenfilename()
 chunk_size = 4096 # Leseblockgröße für große Dateien
 
 crc_value = 0 # Initialwert für den CRC
-with open(file_path, 'rb') as f:
-   
 
-    while True:
-        chunk = f.read(chunk_size)
-        if not chunk:
-            break
-        # Berechne den CRC für den aktuellen Chunk und update den Gesamtwert
-        crc_value = crc16_func(chunk, crc_value)
 
 with open(file_path, 'rb') as source_file:
+    global original_data
     original_data = source_file.read()
+    crc_stm = crc32_stm(bytearray(original_data))
+    crc_bytes_le = crc_stm.to_bytes(4, byteorder='little')
+    original_data = bytearray(original_data)  # in bytearray umwandeln
+    original_data.extend(crc_bytes_le)        # crc_bytes_le anhängen
+    for i in range(0, len(original_data), chunk_size):
+        chunk = original_data[i:i+chunk_size]
+        crc_value = crc16_func(chunk, crc_value)
     Bytes14and15=len(original_data)%65536
-    f.close
+    source_file.close
 
 print(f"CRC16-Wert für {file_path}: {crc_value:04X}") # Ausgabe als 4-stellige Hexadezimalzahl
 print(f"CRC16-Wert für {file_path}: {Bytes14and15:04X}")
-print(f"CRC16-Wert für {file_path}: {len(original_data)}")
-
 
 
 # Angenommen, dies ist Ihre Hex-Daten-Zeichenkette
@@ -43,16 +99,23 @@ out_file = filedialog.asksaveasfile(mode="wb", defaultextension='.bin')
 #with open(out_file, 'wb') as f:
     # Schreibe die konvertierten Binärdaten
 binary_data = bytes.fromhex(hex_string_data)
-out_file.write(binary_data)
-out_file.write(Bytes14and15.to_bytes(2, byteorder='big'))
-out_file.write(crc_value.to_bytes(2, byteorder='big'))
-binary_data = bytes.fromhex(filling_zeros)
+binary_data = bytearray(binary_data)
+binary_data.extend(Bytes14and15.to_bytes(2, byteorder='big'))
+binary_data.extend(crc_value.to_bytes(2, byteorder='big'))
+binary_data.extend(bytes.fromhex(filling_zeros))
+binary_data.extend(original_data)
+#out_file.write(binary_data)
+#out_file.write(Bytes14and15.to_bytes(2, byteorder='big'))
+#out_file.write(crc_value.to_bytes(2, byteorder='big'))
+#binary_data = bytes.fromhex(filling_zeros)
 out_file.write(binary_data)
 
-with open(file_path, 'rb') as source_file:
-    binary_data = source_file.read()
+#with open(file_path, 'rb') as source_file:
+#    binary_data = source_file.read()
 
 # Writing the data of the previous file in the original file
 # with open(out_file, 'ab') as destination_file:
-out_file.write(binary_data)
+
+#out_file.write(binary_data)
+
 out_file.close()
